4830562e7e7d286ac759a0d7f6add54faff71da5
krishna <krishnateja262@gmail.com>
2024-07-18 18:24:30 +0530

    adding serialisation and deserialisation for u256

--- utils/Felt.go
+++ utils/Felt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/NethermindEth/juno/core/felt"
@@ -41,15 +42,15 @@ func HexToFelt(hex string) (*felt.Felt, error) {
 // - error: an error if any
 func HexArrToFelt(hexArr []string) ([]*felt.Felt, error) {
 
- feltArr := make([]*felt.Felt, len(hexArr))
- for i, e := range hexArr {
-  felt, err := HexToFelt(e)
-  if err != nil {
-   return nil, err
-  }
-  feltArr[i] = felt
- }
- return feltArr, nil
+	feltArr := make([]*felt.Felt, len(hexArr))
+	for i, e := range hexArr {
+		felt, err := HexToFelt(e)
+		if err != nil {
+			return nil, err
+		}
+		feltArr[i] = felt
+	}
+	return feltArr, nil
 
 }
 
@@ -86,4 +87,45 @@ func FeltArrToBigIntArr(f []*felt.Felt) []*big.Int {
 		bigArr = append(bigArr, FeltToBigInt(felt))
 	}
 	return bigArr
+}
+
+// U256ToFelt converts big int an array of Felt objects to represent U256 datat type
+//
+// Parameters:
+// - num: big.Int
+// Returns:
+// - []*felt.Felt: the array of felt.Felt objects
+// - error: an error, if any
+func U256ToFelt(num *big.Int) ([]*felt.Felt, error) {
+	bytes := num.Bytes()
+	if len(bytes) > 32 {
+		return nil, fmt.Errorf("not a valid U256")
+	}
+
+	all := make([]byte, 32)
+	copy(all[32-len(bytes):], bytes[:])
+
+	least := new(felt.Felt).SetBytes(all[16:])
+	significant := new(felt.Felt).SetBytes(all[0:16])
+	return []*felt.Felt{least, significant}, nil
+}
+
+// FeltArrToU256 array of Felt objects that represents U256 data type to big.Int
+//
+// Parameters:
+// - arr: []*felt.Felt
+// Returns:
+// - *big.Int: big.Int representation of U256
+// - error: an error, if any
+func FeltArrToU256(arr []*felt.Felt) (*big.Int, error) {
+	if len(arr) != 2 {
+		return nil, fmt.Errorf("not a valid felt array for U256 conversion")
+	}
+
+	significant := arr[1].Bytes()
+	least := arr[0].Bytes()
+	res := make([]byte, 32)
+	copy(res[0:16], significant[16:])
+	copy(res[16:], least[16:])
+	return new(big.Int).SetBytes(res), nil
 }
